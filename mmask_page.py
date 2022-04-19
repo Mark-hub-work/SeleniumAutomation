cc1827c279a0581365646aea7a995cd90880bb74
MarkS <Mark.Sollazzo@hotmail.com>
2022-04-19 13:20:56 -0400

    updated after cryptoblades new UI

--- mmask_page.py
+++ mmask_page.py
@@ -1,8 +1,5 @@
-import time
 from base_element import BaseElement
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 class MetaMaskPage:
